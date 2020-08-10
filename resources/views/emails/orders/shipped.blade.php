27d604cc5c2d470a323bf3b94de9e5eb2e369b65
Julian Prasetyo <picobug.jp@gmail.com>
2020-08-10 21:26:24 +0700

    Update Layout Email

--- resources/views/emails/orders/shipped.blade.php
+++ resources/views/emails/orders/shipped.blade.php
@@ -13,10 +13,11 @@
 | Data Transaksi |          |
 | ------------- |-------------:|
 | Bank Member | {{ $transaction->bank->nama->nama }} |
+| No {{ $transaction->nilai_depo_id ? 'Rekening' : 'Akun' }} | {{ $transaction->nilai_depo_id ? $transaction->bank->nomor_rekening : $transaction->currency_member->no_account }} |
 | E-Payment / Akun Broker Member | {{ $transaction->currency_member->nama->jenis }} |
-| Kurs Beli  (Rp) | {{ $transaction->jenis_currency->beli }} |
+| Kurs {{ $transaction->nilai_depo_id ? 'Beli' : 'Jual' }}  (Rp) | {{ $transaction->jenis_currency->{$transaction->nilai_depo_id?'beli':'jual'} }} |
 | Diskon Rate (Rp) | {{ $transaction->jenis_currency->diskon ?? '-' }} |
-| Jumlah Beli  (USD) | {{ $transaction->jenis_currency->min_trans }} |
+| Jumlah {{ $transaction->nilai_depo_id ? 'Beli' : 'Jual' }}  (USD) | {{ $transaction->jenis_currency->min_trans }} |
 | Total Transfer (Rp) | {{ $transaction->total }} |
 @endcomponent
 
